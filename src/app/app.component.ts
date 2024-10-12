d37a2bf2a2ef0d3f50e4bc7701000451e16a4e26
klylton gomes de souza <klyltongomesdesouza@MacBook-Pro-de-klylton.local>
2024-10-12 16:18:59 -0300

    pagina de login , components Button , pages login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from "./pages/login/login.component";
+import { ButtonComponent } from "./components/button/button.component";
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent, ButtonComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { ButtonComponent } from "../../components/button/button.component";
+import { Router, RouterOutlet } from '@angular/router';
+import { NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,ButtonComponent,NgClass,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+textButton: any;
+userName: string;
 
+constructor(private rota: Router){
+
+}
+
+
+login(){
+  sessionStorage.setItem('user', this.userName);
+
+  this.rota.navigate(['home'])
+
+}
+  
 }
